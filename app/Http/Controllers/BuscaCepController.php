90d119a36c7d59266de6eead39128c6c4e0ba2a5
Gustavo Rosolen Brigatti <gustavo.brigatti13@gmail.com>
2022-03-28 20:14:26 -0300

    exibição e validação dos resultados da busca do cep

--- app/Http/Controllers/BuscaCepController.php
+++ app/Http/Controllers/BuscaCepController.php
@@ -12,25 +12,31 @@ class BuscaCepController extends Controller
     }
 
     public function buscaCep(Request $request){
+        $erro = false;
         if (Endereco::where('cep', $request->cep)->count() == 0){
             $xml = simplexml_load_file('https://viacep.com.br/ws/'. $request->cep . '/xml/');
             $endereco = new Endereco();
-            $endereco->cep = $xml->cep;
-            $endereco->logradouro = $xml->logradouro;
-            $endereco->complemento = $xml->complemento;
-            $endereco->bairro = $xml->bairro;
-            $endereco->localidade = $xml->localidade;
-            $endereco->uf = $xml->uf;
-            $endereco->ibge = $xml->ibge;
-            $endereco->gia = $xml->gia;
-            $endereco->ddd = $xml->ddd;
-            $endereco->siafi = $xml->siafi;
-            $endereco->save();
+            if (!$xml->erro){
+                $endereco->cep = (string)$xml->cep;
+                $endereco->logradouro = (string)$xml->logradouro;
+                $endereco->complemento = (string)$xml->complemento;
+                $endereco->bairro = (string)$xml->bairro;
+                $endereco->localidade = (string)$xml->localidade;
+                $endereco->uf = (string)$xml->uf;
+                $endereco->ibge = (string)$xml->ibge;
+                $endereco->gia = (string)$xml->gia;
+                $endereco->ddd = (string)$xml->ddd;
+                $endereco->siafi = (string)$xml->siafi;
+                $endereco->save();
+            }else{
+                $erro = true;
+            }
         }else{
             $endereco = Endereco::where('cep', $request->cep)->first();
         }
         return response()->json([
-            'endereco' => $endereco
+            'endereco' => $endereco,
+            'erro' => $erro
         ]);
     }
 
